8cf2f6baaab2907698ca5474672dd958617eb57d
dlxor <dlxor@LEE>
2024-01-17 01:16:55 +0900

    숙제

--- hgjava/src/com/yedam/todo/Todo0116.java
+++ hgjava/src/com/yedam/todo/Todo0116.java
@@ -5,9 +5,10 @@ import java.util.Scanner;
 public class Todo0116 {
 	public static void main(String[] args) {
 		Scanner scn = new Scanner(System.in);
+	
+		//System.out.println("값을 입력>>> ");
+		//String input = scn.nextLine();
 		
-		System.out.println("값을 입력>>> ");
-		String input = scn.nextLine();
 		// while문 써서 계속 반복하는 조건에서
 		
 		// input.equals("quit")
@@ -15,5 +16,15 @@ public class Todo0116 {
 		//입력값은 홍길동 입니다.
 		
 		// quit 값이 들어오면 반복문이 종료되면서 콘솔창에 <<end of prog>>
+
+		while(true) {
+	         System.out.print("홍길동");
+	         String input=scn.nextLine();
+	         if(input.equals("quit")) {
+	            break;
+	         }
+	      }
+	      System.out.println("<<end of prog>>");
+			
 	}
 }
